1456dba3939f6fdc0e8e594cbc4aa95c7717a139
Ismael Cordon Dominguez <icordondominguez@gmail.com>
2020-03-17 14:38:01 +0100

    cambio en la cadena de conexion remota

--- server/config/config.js
+++ server/config/config.js
@@ -18,7 +18,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev'){
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://Ismael:Mongo2020@cluster0-3rvx3.mongodb.net/cafe'
+    urlDB = 'mongodb+srv://Ismael:Mongo2020@cluster0-3rvx3.mongodb.net/cafe?retryWrites=true&w=majority'
 }
 
 process.env.URLDB = urlDB;
